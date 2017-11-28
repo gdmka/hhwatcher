54447d998ac7d84cbdc99b92b454bb9e3243f2b0
Alexey Haidamaka <ahaidamaka@gmail.com>
2017-11-28 09:54:00 +0300

    use safe navigation

--- hhWatcher/Determinator.cs
+++ hhWatcher/Determinator.cs
@@ -52,12 +52,12 @@ namespace hhWatcher
 
         public static void Parse(HtmlDocument doc)
         {
+            string pos = doc.DocumentNode.SelectSingleNode($"//span[contains(@class, '{posSelector}')]")?.InnerHtml;
 
-            HtmlNode pos = doc.DocumentNode.SelectSingleNode($"//span[contains(@class, '{posSelector}')]");
             if (pos != null)
             {
                 _doc = doc;
-                PosChanged(pos.InnerHtml);
+                PosChanged(pos);
             }
         }
 
